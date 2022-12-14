2c6695f3d74e27079567091c45cc30fe280afc69
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-12-14 20:48:36 +0100

    refactor(layout): Delete unnecessary head.tsx

--- front/app/head.tsx
+++ front/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>This is test header</title>
+      <title>Management System</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
     </>
   );
