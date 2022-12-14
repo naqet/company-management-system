27027bc328ca7e84ee591b1d6aa8d06c42c4af3f
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-12-14 15:00:49 +0100

    feat(prettier): Add prettier

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,19 +1,19 @@
-import { Roboto, Poppins } from '@next/font/google';
+import { Roboto, Poppins } from "@next/font/google";
 import "../styles/globals.css";
 
-const roboto = Roboto({ variable: "--font-roboto", weight: '400' });
-const poppins = Poppins({ variable: "--font-poppins", weight: ['400', '600'] });
+const roboto = Roboto({ variable: "--font-roboto", weight: "400" });
+const poppins = Poppins({ variable: "--font-poppins", weight: ["400", "600"] });
 
 export default function RootLayout({
-	// Layouts must accept a children prop.
-	// This will be populated with nested layouts or pages
-	children,
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" className={`dark ${roboto.variable} ${poppins.variable}`}>
-			<body >{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={`dark ${roboto.variable} ${poppins.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
 }
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,3 +1,3 @@
 export default function HomePage() {
-	return <h1>This is home page</h1>
+  return <h1>This is home page</h1>;
 }
