08b232a86b2b5250b7d2491ceb5fe729972c4a10
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-12-14 19:38:50 +0100

    refactor(layout): Added different root layout for auth

--- front/app/(auth)/login/page.tsx
+++ front/app/(auth)/login/page.tsx
@@ -1,9 +1,5 @@
 import LoginPanel from "../../../components/auth/LoginPanel";
 
 export default function LoginPage() {
-  return (
-    <main className="grid place-items-center h-screen">
-      <LoginPanel />
-    </main>
-  );
+  return <LoginPanel />;
 }
--- front/app/(auth)/remindPassword/page.tsx
+++ front/app/(auth)/remindPassword/page.tsx
@@ -1,9 +1,5 @@
 import RemindPasswordPanel from "../../../components/auth/RemindPasswordPanel";
 
 export default function RemindPasswordPage() {
-  return (
-    <main className="grid place-items-center h-screen">
-      <RemindPasswordPanel />
-    </main>
-  );
+  return <RemindPasswordPanel />;
 }
