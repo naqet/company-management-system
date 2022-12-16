0f3f207c2f03521ab6abc683d4580473ee933c18
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-12-16 18:17:54 +0100

    feat(dashboard): Add responsive projects layout

--- front/app/(app)/page.tsx
+++ front/app/(app)/page.tsx
@@ -42,14 +42,18 @@ export default function HomePage() {
           here are your current projects
         </span>
       </h1>
-      <div className="border-1 dark:border-slate-800 p-4 rounded-lg w-full grid gap-4">
+      <div className="border-1 dark:border-slate-800 p-4 rounded-lg w-full grid gap-4 @container">
         <h2 className="font-semibold dark:text-slate-400">Projects</h2>
-        <ul className="flex flex-wrap gap-4">
+        <ul className="flex flex-wrap gap-4 @lg:grid @lg:grid-cols-2 @3xl:grid-cols-3 @6xl:grid-cols-4">
           {projects.map((project) => (
-            <li key={project.id} className="w-full">
+            <li
+              key={project.id}
+              className="w-full @lg:row-span-1 @lg:col-span-1 "
+            >
               <ProjectPreview project={project} />
             </li>
           ))}
+          {/* TODO: handle adding new project */}
         </ul>
       </div>
     </main>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,20 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: "class",
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			borderWidth: {
-				1: "1px"
-			},
-			fontFamily: {
-				roboto: ['var(--font-roboto)'],
-				poppins: ['var(--font-poppins)'],
-			}
-		},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      borderWidth: {
+        1: "1px",
+      },
+      fontFamily: {
+        roboto: ["var(--font-roboto)"],
+        poppins: ["var(--font-poppins)"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/container-queries")],
+};
